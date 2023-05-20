84b5a2886d38c469447ca7869013ee152f5631d0
ryanonthedrums <123711058+ryanonthedrums@users.noreply.github.com>
2023-05-20 06:11:39 +0000

    Pending changes exported from your codespace

--- q1.cpp
+++ q1.cpp
@@ -1,7 +1,23 @@
 #include "q1.hpp"
 
+// double sumScores(const Student *ptr);
+
 Student *makeStudent(int N)
 {
+	ifstream ifs;
+	int i = 0;
+	ifs.open("grades.txt");
+	Student *ptr = new Student[N];
+	while (ifs >> (ptr + i)->first >> (ptr + i)->last >> (ptr + i)->ssn)
+	{
+		for (int sci = 0; sci < SCN; sci++)
+		{
+			ifs >> (ptr + i)->score.sc[sci];
+		}
+		ifs >> (ptr + i)->score.grade;
+		i += 1;
+	}
+	ifs.close();
 
 	return ptr;
 }
@@ -27,4 +43,18 @@ void printStudent(Student *ptr, int N)
 }
 void sortStudent(Student *ptr, int N, string field, int asc)
 {
+    // int i, j;
+    // for (i = 0; i < N - 1; i++)
+    //     for (j = 0; j < N - i - 1; j++)
+    //         if (sumScores(ptr[j]) > sumScores(ptr[j + 1]))
+    //             swap(ptr[j], ptr[j + 1]);
+}
+
+double sumScores(const Student *ptr)
+{
+	// double sum = 0;
+	// for (int i = 0; i < SCN; i++) {
+    //     sum += ptr.scores[i];
+    // }
+    // return sum;
 }
